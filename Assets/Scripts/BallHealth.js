e4cee88027cea12e1193297acb79bc9627565523
Adarsh Baluni <adarshbaluni@gmail.com>
2015-09-19 01:19:51 -0700

    Unity game and its file added. First commit.

---
+++ Assets/Scripts/BallHealth.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+var maxFallDistance = -10 ;
+
+function Update () {
+	if(transform.position.y <= maxFallDistance){
+		Application.LoadLevel("Level01")	;
+	}
+}
---
+++ Assets/Scripts/CoinPickup.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var count : int = 0 ;
+function OnTriggerEnter (info : Collider) {
+	if(info.tag == "Player"){
+	
+		//Debug.Log("Coin Collected") ;
+		count = count + 1 ;
+		Debug.Log(" Count incremented") ;
+		Destroy(gameObject);
+		GUI.Label (Rect (10, 30, 1000, 20),"Coins: " + count.ToString());
+	}
+}
